b715aa77d0a0e46ec6a94161d0b8a07538af48a6
lavanyauj <86716651+lavanyauj@users.noreply.github.com>
2021-07-14 19:55:50 +0530

    Update FormSignup.js

--- src/FormSignup.js
+++ src/FormSignup.js
@@ -1,41 +1,47 @@
-  
+
 import React from 'react';
 import validate from './validateInfo';
 import useForm from './useForm';
-import {select,MenuItem} from '@material-ui/core';
 import './Form.css';
+import {TextField} from '@material-ui/core'
+import Form from './Form';
 
 const FormSignup = ({ submitForm }) => {
-  const { handleChange, handleSubmit, values, errors } = useForm(
+  const { handleChange, handleSubmit, values, errors, dropdown, setDropdown } = useForm(
     submitForm,
-    validate
-  );
+    validate  );
+
 
   return (
     <div className='form-content-right'>
+      <p id="login-welcome"> Way to ZD!</p>
       <form onSubmit={handleSubmit} className='form' noValidate>
-        <h1> CHOOSE YOUR ZILLION FAMILY NAME</h1>
-              <select value="unborn egg">
-              <h1 className="dropdown-login">unborn egg</h1>
-
-        <MenuItem value={1} >Neonate Achiever</MenuItem>
-        <MenuItem value={1} >Tenderfoot Activist</MenuItem>
-        <MenuItem value={1} >Human Dynamo</MenuItem>
-        <MenuItem value={1} >Stardom Dreamer</MenuItem>
-        <MenuItem value={1} >I'm Tireless Seeker</MenuItem>
-        <MenuItem value={1} >Brobdingnagian Engine</MenuItem>
-        <MenuItem value={1} >Zillionaire</MenuItem>
-        <MenuItem value={1} >Human Dynamo</MenuItem>
-
-      </select>
-        <p>GENDER</p>
-         <input type="radio" name="gender" value="male"/>
-        <label>Male </label>
-        <input type="radio" name="gender" value="male"/>
-        <label>Female </label>
-        <p>EMAIL ADDRESS/PHONE NUMBER</p>
+        <h1>
+          CHOOSE YOUR ZILLION FAMILY NAME
+        </h1>
+        <div className='dropdown'>
+        <select value={dropdown} onChange={(e)=>{setDropdown(e.target.value)}}>
+          <option value="unborn egg">Unborn egg</option>
+          <option value="neonate achiever">Neonate Achiever</option>
+          <option value="tenderfoot activist">Tenderfoot Activist</option>
+          <option value="human dynamo">Human Dynamo</option>
+          <option value="stardom dreamer">Stardom Dreamer</option>
+          <option value="i'm tireless seeker">I'm Tireless Seeker</option>
+          <option value="brobdingnagian engine">Brobdingnagian Engine</option>
+          <option value="zillionaire">Zillionaire</option>
+          <option value="human dynamo">Human Dynamo</option>
+        </select>
+        </div>
+        <div className="gender">
+          <label>GENDER </label>
+            <input type="radio" value="male" name="gender" /> Male 
+            <input type="radio" value="female" name="gender" /> Female
+        </div>
+        <h1>EMAIL ADDRESS/PHONE NUMBER
+        <TextField />
+        </h1>
         <button className='form-input-btn' type='submit'>
-          update
+          <h6>Update</h6>
         </button>
       </form>
     </div>
